abf5c421da4a52eeec3d286573dff2686bf1231a
Steven Jing <esteban@stevens-air.lan>
2021-01-04 23:45:43 -0800

    add event input fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import './App.css';
+import { useState } from 'react';
+import {
+  Button,
+  Form,
+} from 'semantic-ui-react';
 
 function App() {
+  const [eventName, setEventName] = useState('');
+  const [eventDate, setEventDate] = useState(new Date());
+  const [eventTime, setEventTime] = useState('');
+
   return (
     <div className="App">
-
+      <EventInput eventName={eventName}></EventInput>
     </div>
+  );
+}
+
+function EventInput({ eventName }: { eventName : string }) {
+  return (
+    <Form>
+      <Form.Field>
+        <label>Event Name</label>
+        <input
+          placeholder='Name'
+          value={eventName}
+        />
+      </Form.Field>
+      <Form.Field>
+        <label>Date</label>
+        <input placeholder='Date' />
+      </Form.Field>
+      <Form.Field>
+        <label>Time</label>
+        <input placeholder='Time' />
+      </Form.Field>
+      <Button type='submit'>Submit</Button>
+    </Form>
   );
 }
 
